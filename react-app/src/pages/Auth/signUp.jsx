75c364709748d2b85bd2edc96bab5641e5878dc0
Aakanksha <sedhaiaakanksha@gmail.com>
2026-04-21 23:44:03 +0545

    Environment Setup

---
+++ react-app/src/pages/Auth/signUp.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const signUp = () => {
+  return <div>SignUp</div>;
+};
+export default Login;
---
+++ react-app/src/pages/Dashboard/Income.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const income = () => {
+  return <div>Income</div>;
+};
+
+export default Income;
