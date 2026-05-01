d410316ff03ccb1285bf66ec16cc9c27662ae560
Aakanksha <sedhaiaakanksha@gmail.com>
2026-05-02 00:51:21 +0545

    created login form

--- react-app/src/pages/Auth/Login.jsx
+++ react-app/src/pages/Auth/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Input from "../../components/Inputs/input";
 import AuthLayout from "../../components/layouts/AuthLayout";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -9,10 +10,24 @@ const Login = () => {
   const [error, setError] = useState(null);
 
   const navigate = useNavigate();
-
   //handle login
 
-  const handleLogin = async (e) => {};
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+    setError("");
+
+    //Login API call
+  };
   return (
     <AuthLayout>
       <div className="lg:w-[70%] max-h-full flex flex-col justify-center mt-50">
@@ -29,6 +44,26 @@ const Login = () => {
             placeholder="johndoe@example.com"
             type="text"
           />
+          <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+            type="password"
+          />
+
+          {error && <p className="text-red-500 text-xs pb-2.5 ">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            Login
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/SignUp">
+              SignUp
+            </Link>
+          </p>
         </form>
       </div>
     </AuthLayout>
