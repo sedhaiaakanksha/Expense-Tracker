dca91bd48d56e094c561a39af2ff0c4452d6817c
Aakanksha <sedhaiaakanksha@gmail.com>
2026-05-03 23:52:22 +0545

    Handling signup

--- react-app/src/pages/Auth/signUp.jsx
+++ react-app/src/pages/Auth/signUp.jsx
@@ -1,6 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../../components/Inputs/input";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { useNavigate, Link } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("null");
+
+  const [error, setError] = useState("null");
+
+  const navigate = useNavigate();
+
+  //Handle signUp Form submit
+
+  const handleSignUp = async (e) => {};
+  return (
+    <div>
+      <AuthLayout>
+        <div className="lg:w-full h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+          <h3 className="text-xl font-semibold text-black">
+            Create an Account
+          </h3>
+          <p className="text-xs text-slate-700 mt-1.25 mb-6">
+            Join us today by entering your details below.
+          </p>
+
+          <form onSubmit={handleSignUp}>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <Input
+                value={fullName}
+                onChange={({ target }) => setFullName(target.value)}
+                label="Full Name"
+                placeholder="John Doe"
+                type="text"
+              />
+              <div className="row-span-2"></div>
+              <Input
+                value={email}
+                onChange={({ target }) => setEmail(target.value)}
+                label="Email Address"
+                placeholder="johndoe@example.com"
+                type="text"
+              />
+
+              <Input
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholder="Min 8 Characters"
+                type="password"
+              />
+            </div>
+          </form>
+        </div>
+      </AuthLayout>
+    </div>
+  );
 };
 export default SignUp;
