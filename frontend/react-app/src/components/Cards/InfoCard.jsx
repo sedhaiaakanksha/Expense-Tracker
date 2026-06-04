7a3b4a43f1f8b2f50bf6272ae8860fab72762395
Aakanksha <sedhaiaakanksha@gmail.com>
2026-06-04 23:49:06 +0545

    Fetched Dashboard Data

---
+++ frontend/react-app/src/components/Cards/InfoCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InfoCard = ({ icon, label, color }) => {
+  return <div>InfoCard</div>;
+};
+
+export default InfoCard;
--- frontend/react-app/src/pages/Dashboard/Home.jsx
+++ frontend/react-app/src/pages/Dashboard/Home.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
 import useUserAuth from "../../hooks/useUserAuth";
 import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import InfoCard from "../../components/Cards/InfoCard";
+
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+
+import { IoMdCard } from "react-icons/io";
 
 const Home = () => {
   useUserAuth();
@@ -12,10 +19,43 @@ const Home = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const fetchDashboard = async () => {};
+  const fetchDashboardData = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`,
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong. Please Try again", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  }, []);
+
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div className="my-5 mx-auto">Home</div>
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance"
+            value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+            color="bg-primary"
+          />
+        </div>
+      </div>
     </DashboardLayout>
   );
 };
