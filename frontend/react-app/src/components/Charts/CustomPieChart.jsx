d9cd2b6f7e072b6d30b9e448de02817864999b08
Aakanksha <sedhaiaakanksha@gmail.com>
2026-06-05 02:05:41 +0545

    Working on finincial overview section

---
+++ frontend/react-app/src/components/Charts/CustomPieChart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CustomPieChart = ({
+  data,
+  label,
+  totalAmount,
+  colors,
+  AshowTextnchor,
+}) => {
+  return <div>CustomPieChart</div>;
+};
+
+export default CustomPieChart;
--- frontend/react-app/src/pages/Dashboard/Home.jsx
+++ frontend/react-app/src/pages/Dashboard/Home.jsx
@@ -12,6 +12,8 @@ import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
 import { IoMdCard } from "react-icons/io";
 import TransactionInfoCard from "../../components/Cards/TransactionInfoCard";
 
+import FinanceOverview from "../../components/Dashboard/FinanceOverview";
+
 const Home = () => {
   useUserAuth();
 
@@ -49,7 +51,7 @@ const Home = () => {
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <InfoCard
             icon={<IoMdCard />}
             label="Total Balance"
@@ -70,12 +72,18 @@ const Home = () => {
             value={addThousandSeparator(dashboardData?.totalExpense || 0)}
             color="bg-red-500"
           />
-        </div>
+        </div> */}
 
         <div className="grid grid-cols-1 md:grid-cols-1 gap-6 mt-6">
           <RecentTransactions
             transactions={dashboardData?.recentTransactions}
             onSeeMore={() => navigate("/expense")}
+          />
+
+          <FinanceOverview
+            totalBalance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpense={dashboardData?.totalExpense || 0}
           />
         </div>
       </div>
