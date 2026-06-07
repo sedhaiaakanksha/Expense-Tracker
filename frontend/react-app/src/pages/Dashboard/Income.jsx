72a6698479ab97a90f300bd218a10bb54118c2da
Aakanksha <sedhaiaakanksha@gmail.com>
2026-06-08 00:22:37 +0545

    completed income list section

--- frontend/react-app/src/pages/Dashboard/Income.jsx
+++ frontend/react-app/src/pages/Dashboard/Income.jsx
@@ -6,6 +6,8 @@ import { API_PATHS } from "../../utils/apiPaths";
 import Modal from "../../components/layouts/Modal";
 import AddIncomeForm from "../../components/Income/AddIncomeForm";
 import { toast } from "react-hot-toast";
+import IncomeList from "../../components/Income/IncomeList";
+import DeleteAlert from "../../components/layouts/DeleteAlert";
 
 const Income = () => {
   const [incomeData, setIncomeData] = useState([]);
@@ -97,6 +99,13 @@ const Income = () => {
               onAddIncome={() => setOpenAddIncomeModal(true)}
             />
           </div>
+          <IncomeList
+            transactions={incomeData}
+            onDelete={(id) => {
+              setOpenDeleteAlert({ show: true, data: id });
+            }}
+            onDownload={handleDownloadIncomeDetails}
+          />
         </div>
         <Modal
           isOpen={openAddIncomeModal}
@@ -104,6 +113,17 @@ const Income = () => {
           title="Add Income"
         >
           <AddIncomeForm onAddIncome={handleAddIncome} />
+        </Modal>
+
+        <Modal
+          isOpen={openDeleteAlert.show}
+          onClose={() => setOpenDeleteAlert({ show: false, data: null })}
+          title="Delete Income"
+        >
+          <DeleteAlert
+            content="Are you sure you want to delete this income detail?"
+            onDelete={() => deletIncome(openDeleteAlert.data)}
+          />
         </Modal>
       </div>
     </DashboardLayout>
